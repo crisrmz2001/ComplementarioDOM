a2945f441d5761aa81160264e529305cbe0faa17
Cristhian Ramírez <cristhian>
2022-04-18 14:39:31 -0300

    DOM

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector("main .container .product-container .moto1").textContent = "Benelli 752 S"
+document.querySelector("main .container .product-container .moto2").textContent = "Benelli 502 C"
+document.querySelector("main .container .product-container .moto3").textContent = "Benelli Leoncino 250"
+document.querySelector("main .container .product-container .moto4").textContent = "Benelli Trk 502"
+document.querySelector("main .container .product-container .moto5").textContent = "Benelli Trk 502 X"
+document.querySelector("main .container .product-container .moto6").textContent = "Benelli Trk 251 Abs"
+document.querySelector("main .container .product-container .precio1").textContent = "2.750.000 $"
+document.querySelector("main .container .product-container .precio2").textContent = "1.920.000 $"
+document.querySelector("main .container .product-container .precio3").textContent = "885.900 $"
+document.querySelector("main .container .product-container .precio4").textContent = "2.215.000 $"
+document.querySelector("main .container .product-container .precio5").textContent = "2.510.000 $"
+document.querySelector("main .container .product-container .precio6").textContent = "993.900 $"
+//const arrayNombres = ["Benelli 752 S","Benelli 502 C","Benelli Leoncino 250","Benelli Trk 502","Benelli Trk 502","Benelli Trk 251 Abs"];
+//const arrayPrecios = ["2.750.000 $","1.920.000 $","885.900 $","2.215.000 $","2.510.000 $","993.900 $"]
+
